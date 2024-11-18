c2cc532badc0f1b1693af1c4b25f4b79d3721fe6
Rogerlabastera <Rogerlabastera>
2024-11-18 19:37:26 +0800

    Commit Transaction Purchase and Transaction Receive

---
+++ app/src/main/java/com/project/pawsitivevibes/transactionReceive.kt
@@ -0,0 +1,20 @@
+package com.project.pawsitivevibes
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class transactionReceive : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_transaction_receive)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
